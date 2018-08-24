18c65650fcb7e933ad90d3da534c56f29c1f2c39
DavisDevelopment <epikit@gmail.com>
2018-08-24 07:00:11 -0500

    created new pmdb.core.ds.FAVLTree class
    a subclass of AVLTree meant to represent a functionally-defined tree

---
+++ pmdb/core/ds/FAVLTree.hx
@@ -0,0 +1,28 @@
+package pmdb.core.ds;
+
+import haxe.ds.Option;
+
+import tannus.math.TMath as Math;
+import Slambda.fn;
+
+import pmdb.core.ds.AVLTree;
+import pmdb.core.Comparator;
+
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.ds.IteratorTools;
+using tannus.FunctionTools;
+using tannus.async.OptionTools;
+
+class FAVLTree<K, V> extends AVLTree<K, V> {
+    /* Constructor Function */
+    public function new(options) {
+        super( options );
+    }
+
+/* === Methods === */
+
+/* === Fields === */
+
+}
+
