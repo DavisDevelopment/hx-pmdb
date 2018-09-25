146a806abfde611d87d5ff668418fda1ce64dd60
DavisDevelopment <epikit@gmail.com>
2018-09-25 01:12:58 -0500

    started the process of writing tests and benchmarks that are actually a part of the repo

---
+++ test/TestAll.hx
@@ -0,0 +1,17 @@
+package ;
+
+import utest.Assert;
+//import utest.UTest;
+
+class TestAll {
+    /* Constructor Function */
+    public function new() {
+        init();
+    }
+
+    function init() {
+        
+    }
+
+    public static function main() new TestAll();
+}
