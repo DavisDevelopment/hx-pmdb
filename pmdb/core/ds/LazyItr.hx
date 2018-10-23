36472a2cf8dae942b8c42d06948fe57bdc4b0609
DavisDevelopment <epikit@gmail.com>
2018-10-23 01:17:37 -0500

    created new pmdb.core.ds.LazyItr.LazyItrIterator class

--- pmdb/core/ds/LazyItr.hx
+++ pmdb/core/ds/LazyItr.hx
@@ -9,3 +9,23 @@ class LazyItrStep<T> {
     @:optional public var done(default, null): Bool;
     @:optional public var value(default, null): T;
 }
+
+class LazyItrIterator<T> {
+    public function new(li: LazyItr<T>):Void {
+        lazy = li;
+        state = lazy.next();
+    }
+
+    public function hasNext() {
+        return state == null ? false : !(state.done != null ? state.done : false);
+    }
+
+    public function next():T {
+        var tmp = state.value;
+        state = lazy.next();
+        return tmp;
+    }
+
+    public var lazy: LazyItr<T>;
+    var state: Null<LazyItrStep<T>>;
+}
