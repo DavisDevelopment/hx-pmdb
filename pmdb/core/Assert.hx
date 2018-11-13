bd687498f060e414fd51a1dee8c8196df62bff68
DavisDevelopment <epikit@gmail.com>
2018-11-12 23:20:05 -0600

    Added documentation comments and additional type info to pmdb.core.Asserts

--- pmdb/core/Assert.hx
+++ pmdb/core/Assert.hx
@@ -7,12 +7,20 @@ import pmdb.core.Error;
 import haxe.PosInfos;
 
 class Assert {
-    public static inline function assert(condition:Lazy<Bool>, ?msg:Lazy<String>, ?pos:PosInfos):Void {
+    /**
+      throws an error if [condition] is not met
+     **/
+    public static inline function assert<E>(condition:Lazy<Bool>, ?msg:Lazy<E>, ?pos:PosInfos):Void {
+        #if debug
         if (!condition.get())
             _toss(msg, pos);
+        #end
     }
 
-    public static inline function assertThrows(fn:Void->Void, ?msg:Lazy<String>, ?pos:PosInfos):Void {
+    /**
+      throws an error if [fn] doesn't cause an error to be thrown
+     **/
+    public static inline function assertThrows<E>(fn:Void->Void, ?msg:Lazy<E>, ?pos:PosInfos):Void {
         try {
             fn();
             _toss(msg, pos);
@@ -22,8 +30,16 @@ class Assert {
         }
     }
 
-    private static inline function _toss(?s:Lazy<String>, ?pos:PosInfos) {
-        throw new AssertionFailureError(s, pos);
+    /**
+      utility method for throwing an exception
+     **/
+    private static function _toss<E>(?s:Lazy<E>, ?pos:PosInfos) {
+        if (s == null || (s.get() is String)) {
+            throw new AssertionFailureError(s.map(e -> cast(e, String)), pos);
+        }
+        else {
+            throw s.get();
+        }
     }
 }
 
