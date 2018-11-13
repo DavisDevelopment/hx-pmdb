e919e45ec8c54fb764dcbc1c5c43fcf4eeacdd98
DavisDevelopment <epikit@gmail.com>
2018-11-12 23:25:08 -0600

    redesigned LazyItr<T> as abstract

--- pmdb/core/ds/LazyItr.hx
+++ pmdb/core/ds/LazyItr.hx
@@ -1,14 +1,31 @@
 package pmdb.core.ds;
 
-interface LazyItr<T> {
+@:forward
+abstract LazyItr<T> (LazyIterator<T>) from LazyIterator<T> to LazyIterator<T> {
+    @:to
+    public inline function iterate():LazyItrIterator<T> {
+        return new LazyItrIterator( this );
+    }
+
+    @:to
+    public inline function iterator():Iterator<T> {
+        return cast iterate();
+    }
+}
+
+interface LazyIterator<T> {
     function next():LazyItrStep<T>;
 }
 
-@:structInit
-class LazyItrStep<T> {
-    @:optional public var done(default, null): Bool;
-    @:optional public var value(default, null): T;
-}
+//@:structInit
+//class LazyItrStep<T> {
+    //@:optional public var done(default, null): Bool;
+    //@:optional public var value(default, null): T;
+//}
+typedef LazyItrStep<T> = {
+    ?done: Bool,
+    ?value: T
+};
 
 class LazyItrIterator<T> {
     public function new(li: LazyItr<T>):Void {
@@ -16,14 +33,20 @@ class LazyItrIterator<T> {
         state = lazy.next();
     }
 
-    public function hasNext() {
-        return state == null ? false : !(state.done != null ? state.done : false);
+    public inline function hasNext() {
+        return state == null ? false : !patchState(state).done;
     }
 
-    public function next():T {
+    public inline function next():T {
         var tmp = state.value;
         state = lazy.next();
         return tmp;
+    }
+
+    function patchState(step: LazyItrStep<T>):LazyItrStep<T> {
+        if (step.done == null)
+            step.done = false;
+        return step;
     }
 
     public var lazy: LazyItr<T>;
