346f8a1ffcf3ba1be19d18b519169ebcb09ecf8d
DavisDevelopment <epikit@gmail.com>
2018-12-20 03:59:31 -0600

    added a 'UStruct' construct to UpdateExpr

--- pmdb/ql/ast/UpdateExpr.hx
+++ pmdb/ql/ast/UpdateExpr.hx
@@ -5,7 +5,9 @@ import pmdb.ql.ast.PredicateExpr;
 
 enum UpdateExpr {
     UNoOp;
+
     UAssign(column:ValueExpr, value:ValueExpr);
+    UStruct(fields: Array<UpdateStructField>);
     UDelete(column: ValueExpr);
     //URename(oldColumn:ValueExpr, newColumn:ValueExpr);
 
@@ -16,4 +18,15 @@ enum UpdateExpr {
 /* === Combinators === */
 
     UBlock(updates: Array<UpdateExpr>);
+}
+
+@:structInit
+class UpdateStructField {
+    public final field: String;
+    public final value: ValueExpr;
+
+    public inline function new(field, value) {
+        this.field = field;
+        this.value = value;
+    }
 }
