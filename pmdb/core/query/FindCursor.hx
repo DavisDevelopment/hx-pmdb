bdf3f55a9c525d065fe1081eb7ca4ef2c82d159b
DavisDevelopment <epikit@gmail.com>
2018-12-28 05:48:17 -0600

    added 'sort' and 'groupBy' methods to FindCursor

--- pmdb/core/query/FindCursor.hx
+++ pmdb/core/query/FindCursor.hx
@@ -7,8 +7,10 @@ import pmdb.ql.ts.*;
 import pmdb.ql.ts.DataType;
 import pmdb.core.ds.AVLTree.AVLTreeNode as Leaf;
 import pmdb.core.ds.LazyItr;
+import pmdb.core.ds.map.Hash;
 import pmdb.core.query.IndexItemCaret;
 import pmdb.core.Store;
+import pmdb.core.query.Value;
 
 import pmdb.ql.*;
 import pmdb.ql.ast.*;
@@ -43,7 +45,7 @@ using pmdb.ql.ast.Predicates;
   TODO: optimize performance for FindCursor<Item> instances where [check] is unspecified
  **/
 @:access(pmdb.core.query.StoreQueryInterface)
-class FindCursor<Item> extends QueryCursor<Item, Array<Item>> {
+class FindCursor<Item> extends QueryCursor<Item, Array<Item>> implements ICursor<Array<Item>> {
     public function new(store, ?crit:Criterion<Item>, compile = false, noInit = false) {
         super( store );
 
@@ -54,6 +56,7 @@ class FindCursor<Item> extends QueryCursor<Item, Array<Item>> {
         this.checkNode = compileCriterion( criterion );
 
         this._compile = compile;
+        this._output = null;
 
         if ( !noInit ) {
             init();
@@ -78,24 +81,70 @@ class FindCursor<Item> extends QueryCursor<Item, Array<Item>> {
       "execute" the FIND operation represented by [this]
      **/
     override function exec():Array<Item> {
-        return getAllNative();
+        if (_output == null)
+            return _output = getAllNative();
+        return _output;
+    }
+
+    /**
+      (SLOW)
+     **/
+    public function map<T>(fn: Item -> T) {
+        return new FCursorTransform(this, function(items) {
+            return items.map( fn );
+        });
+    }
+
+    public function groupBy(expr: Value) {
+        return new ValueGroupedCursorTransform(this, expr);
     }
 
     /**
       skip the Resultset step and just get the results of [this] FIND operation as an array directly
      **/
     public function getAllNative():Array<Item> {
+        // get list of possible (candidate) results
         var res = candidates();
+
+        // perform further filtering by [predicate]
         if (checkNode != null && !(checkNode is pmdb.ql.ast.nodes.NoCheck)) {
             res = res.filter(function(itm: Item):Bool {
-                //return
-                //if ( _compile )
-                    //_filterc(StoreQueryInterface.globalCtx.setDoc(cast itm));
-                //else
-                    //check.eval(StoreQueryInterface.globalCtx.setDoc(cast itm));
                 return this.predicate(qi.ctx.setDoc(itm.asObject()), itm);
             });
         }
+
+        // perform result-sorting
+        if (order != null) {
+            var crit:Array<{key:String, direction:Int}> = [];
+            switch order {
+                case SimpleSort(key, direction):
+                    crit.push({
+                        key: key,
+                        direction: direction
+                    });
+
+                case CompoundSort(_.asObject()=>o):
+                    for (k in o.keys()) {
+                        crit.push({
+                            key: k,
+                            direction: o[k]
+                        });
+                    }
+            }
+            res.sort(function(a:Item, b:Item):Int {
+                var x = a.asObject(), y = b.asObject();
+                var c, cmp;
+                for (i in 0...crit.length) {
+                    c = crit[i];
+                    cmp = c.direction * Arch.compareThings(x.dotGet(c.key), y.dotGet(c.key));
+                    if (cmp != 0)
+                        return cmp;
+                }
+                return 0;
+            });
+        }
+
+        // yield results
         return res;
     }
 
@@ -104,6 +153,11 @@ class FindCursor<Item> extends QueryCursor<Item, Array<Item>> {
         if (itr == null)
             itr = new FindIteration( this );
         return itr;
+    }
+
+    public function sort(criteria: Dynamic<SortOrder>):FindCursor<Item> {
+        this.order = CompoundSort( criteria );
+        return this;
     }
 
     function getBounds():Null<{limit:Int, offset:Int}> {
@@ -122,7 +176,9 @@ class FindCursor<Item> extends QueryCursor<Item, Array<Item>> {
     public var limit(default, null): Null<Int> = null;
     public var offset(default, null): Null<Int> = null;
 
-    public var order(default, null): Null<ResultOrder<Item>> = null;
+    public var order(default, null): Null<ResultSort<Item>> = null;
+
+    public var _output(default, null): Null<Array<Item>> = null;
 
     var _compile(default, null): Bool = false;
     var _filterc(default, null): Null<QueryInterp -> Bool> = null;
@@ -172,45 +228,178 @@ class FindResultPage<T> {
     public var length(default, null): Int;
 }
 
-class ResultOrderRoot<T> {
-    public function sort(c:FindCursor<T>, docs:Array<T>) {
-        throw new NotImplementedError();
+/**
+  represents some form of data which is derived from an ICursor<?> instance
+ **/
+class CursorDerivative<TIn, TOut> implements ICursor<TOut> {
+    /* Constructor Function */
+    public function new(prev) {
+        this.prev = prev;
+        this._output = null;
+    }
+
+/* === Methods === */
+
+    private function _transform(data: TIn):TOut {
+        throw 'Not Implemented';
+    }
+
+    public function exec():TOut {
+        if (_output == null)
+            return _output = _transform(prev.exec());
+        return _output;
+    }
+
+    public function getRoot():Null<FindCursor<Dynamic>> {
+        var c:ICursor<Dynamic> = this;
+        while (c != null) {
+            if ((c is CursorDerivative<Dynamic, Dynamic>)) {
+                c = cast(c, CursorDerivative<Dynamic, Dynamic>).prev;
+            }
+
+            if ((c is FindCursor<Dynamic>)) {
+                return cast c;
+            }
+        }
+        return null;
+    }
+
+/* === Variables === */
+
+    public var prev(default, null): ICursor<TIn>;
+    public var _output(default, null): Null<TOut>;
+}
+
+class CursorPassThrough<T> extends CursorDerivative<T, T> {
+    override function _transform(data: T):T {
+        return data;
     }
 }
 
-class FnResultOrder<T> extends ResultOrderRoot<T> {
-    public function new(fn) {
-        this.fn = fn;
+class FCursorTransform<In, Out> extends CursorDerivative<In, Out> {
+    public function new(cursor, fn:In -> Out) {
+        super( cursor );
+
+        transform = fn;
     }
 
-    override function sort(cursor:FindCursor<T>, docs:Array<T>) {
-        docs.sort( fn );
+/* === Methods === */
+
+    override function _transform(input: In):Out {
+        return transform( input );
     }
 
-    private var fn(default, null): T->T->Int;
+/* === Variables === */
+
+    private var transform(default, null): In -> Out;
 }
 
-class ComparatorResultOrder<T> extends ResultOrderRoot<T> {
-    public function new(comparator) {
-        c = comparator;
+class GroupedCursorTransform<Item, Key> extends CursorDerivative<Array<Item>, Array<Pair<Key, Array<Item>>>> {
+    /* Constructor Function */
+    public function new(cursor) {
+        super(cursor);
     }
 
-    override function sort(cursor:FindCursor<T>, docs:Array<T>) {
-        docs.sort((a, b) -> c.compare(a, b));
+/* === Methods === */
+
+    /**
+      transform [this]'s input into a grouped set
+     **/
+    override function _transform(items: Array<Item>) {
+        var pairs = items.map(i -> new Pair(groupId(i), i));
+        pairs.sort(function(a, b) {
+            return Arch.compareThings(a.key, b.key);
+        });
+        var ga = group( pairs );
+        return ga;
     }
 
-    private var c(default, null): Comparator<T>;
+    private static function group<Key, Item>(pairs: Array<Pair<Key, Item>>):Array<Pair<Key, Array<Item>>> {
+        if (pairs.length == 0) return [];
+
+        var index = 0;
+        var curGroup = new Pair(pairs[0].key, new Array());
+        var groups = [curGroup];
+
+        while (index < pairs.length - 1) {
+            if (Arch.areThingsEqual(curGroup.key, pairs[index].key)) {
+                curGroup.value.push(pairs[index].value);
+            }
+            else {
+                groups.push(curGroup = new Pair(pairs[index].key, new Array()));
+            }
+
+            ++index;
+        }
+
+        return groups;
+    }
+
+    private function groupId(item: Item):Key {
+        throw 'Unimpl';
+    }
+
+/* === Variables === */
+
+    private var dict(default, null): Hash<Key, Array<Item>>;
 }
 
-@:forward
-abstract ResultOrder<T> (ResultOrderRoot<T>) from ResultOrderRoot<T> to ResultOrderRoot<T> {
-    @:from
-    public static inline function ofFunc<T>(fn: T -> T -> Int):ResultOrder<T> {
-        return new FnResultOrder( fn );
+class FGroupedCursorTransform<Item, Key> extends GroupedCursorTransform<Item, Key> {
+    private var key(default, null): Item -> Key;
+    public function new(cursor, fn) {
+        super( cursor );
+
+        this.key = fn;
     }
 
-    @:from
-    public static inline function ofComparator<T>(c: Comparator<T>):ResultOrder<T> {
-        return new ComparatorResultOrder( c );
+    override function groupId(item: Item):Key {
+        return key( item );
     }
+}
+
+class ValueGroupedCursorTransform<Item> extends GroupedCursorTransform<Item, Dynamic> {
+    private var value(default, null): Value;
+    private var get(default, null): Dynamic -> Dynamic;
+
+    public function new(cursor, value) {
+        super(cursor);
+
+        this.value = value;
+        var root = getRoot();
+        if (root == null)
+            throw 'Invalid call';
+        this.value = @:privateAccess this.value.compile( root.qi );
+        var node = this.value.getNode();
+        this.get = node.compile().bind(_, []);
+    }
+
+    override function groupId(item: Item):Dynamic {
+        return get( item );
+    }
+}
+
+class Pair<Key, Value> {
+    public final key: Key;
+    public final value: Value;
+
+    public function new(k, v) {
+        this.key = k;
+        this.value = v;
+    }
+}
+
+interface ICursor<Data> {
+    function exec():Data;
+
+    var _output(default, null):Null<Data>;
+}
+
+enum ResultSort<T> {
+    SimpleSort(field:String, sort:SortOrder);
+    CompoundSort(fields: Dynamic<SortOrder>);
+}
+
+enum abstract SortOrder (Int) from Int to Int {
+    var Asc = -1;
+    var Desc = 1;
 }
