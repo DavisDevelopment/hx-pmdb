5ff6413537e90a54fc460f7b6ce032802c4217de
DavisDevelopment <epikit@gmail.com>
2019-01-09 03:42:14 -0600

    added docs to Outcome type

--- pmdb/core/ds/Outcome.hx
+++ pmdb/core/ds/Outcome.hx
@@ -1,23 +1,37 @@
 package pmdb.core.ds;
 
+@:using(pmdb.core.ds.Outcome.Outcomes)
 enum Outcome<Result, Error> {
     Success(result: Result);
     Failure(error: Error);
 }
 
 class Outcomes {
+    /**
+      check if the given Outcome is a success
+     **/
     public static inline function isSuccess<T, Err>(o:Outcome<T, Err>):Bool {
         return o.match(Success(_));
     }
 
+    /**
+      check if the given Outcome is a failure
+     **/
     public static inline function isFailure<T, Err>(o:Outcome<T, Err>):Bool {
         return o.match(Failure(_));
     }
 
+    /**
+      if [o] is a success Outcome, return its value
+      else if [o] is a failure Outcome, throw its value
+     **/
     public static inline function manifest<T, Err>(o: Outcome<T, Err>):T {
-        return switch ( o ) {
-            case Success(x): x;
-            case Failure(e): throw e;
+        switch ( o ) {
+            case Success( value ): 
+                return value;
+
+            case Failure( error ):
+                throw error;
         }
     }
 }
