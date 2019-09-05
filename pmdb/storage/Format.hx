c3dcb9673310b2a7e2355435f5af695c90e94814
ryan <epikit@gmail.com>
2019-09-05 02:24:11 -0500

    fleshed out the Format module

--- pmdb/storage/Format.hx
+++ pmdb/storage/Format.hx
@@ -15,13 +15,36 @@ class JsonFormat implements IFormat<Dynamic, String> {
     function new() {}
     public static function make() return new JsonFormat();
     public function encode(data: Dynamic):String return Json.stringify( data );
+    public function encodeException(e: Dynamic):String {
+        throw e;
+    }
     public function decode(data: String):Dynamic return Json.parse( data );
 }
 
 class HaxeSerializationFormat implements IFormat<Dynamic, String> {
-    function new() {}
-    public static function make() return new HaxeSerializationFormat();
-    public function encode(data: Dynamic):String return Serializer.run( data );
-    public function decode(data: String):Dynamic return Unserializer.run( data );
+    public function new() {
+        //
+    }
+
+    static final _instance:HaxeSerializationFormat = new HaxeSerializationFormat();
+    public static inline function make() return _instance;
+
+    public function encode(data: Dynamic):String {
+        var s = new Serializer();
+        s.useCache = true;
+        s.serialize(data);
+        return s.toString();
+    }
+
+    public function encodeException(error: Dynamic):String {
+        var s = new Serializer();
+        s.useCache = true;
+        s.serializeException(error);
+        return s.toString();
+    }
+
+    public inline function decode(data: String):Dynamic {
+        return Unserializer.run( data );
+    }
 }
 
