b86a04ef859b248604a33bb2f8900e648c97144e
ryan <epikit@gmail.com>
2019-09-13 01:08:22 -0500

    stub module 'Asyncs'

---
+++ pmdb/utils/Asyncs.hx
@@ -0,0 +1,26 @@
+package pmdb.utils;
+
+import pm.*;
+import pm.async.*;
+import pm.async.Callback;
+import pm.async.Promise;
+import pm.async.Stream;
+
+import haxe.ds.Option;
+// import haxe.Constraints.
+// import pmdb.storage.io.Serialization;
+
+import pm.Functions.fn;
+import pm.Assert.assert;
+
+using pm.Arrays;
+using pm.Iterators;
+using pm.Maps;
+using pm.Options;
+using pm.Functions;
+
+class Asyncs {}
+
+class Promises {
+    
+}
