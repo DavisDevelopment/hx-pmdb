0bd8edc7324ecb2b506fc84dfa1e9fa5a8019ce0
ryan <epikit@gmail.com>
2019-10-23 17:47:14 -0500

    redesigned the IIndex interface

--- pmdb/core/ds/index/IIndex.hx
+++ pmdb/core/ds/index/IIndex.hx
@@ -1,65 +1,56 @@
-package pmdb.core.index;
+package pmdb.core.ds.index;
 
 import pmdb.ql.ts.DataType;
 import pmdb.ql.ast.BoundingValue;
+import pmdb.core.schema.Types.IndexType;
+import pmdb.core.schema.Types.IndexAlgo;
 
 interface IIndex<Key, Item> {
-/* === Fields === */
-    var options(default, null): IndexOptions<Key, Item>;
-    var name(default, null): String;
     var sparse(default, null): Bool;
     var unique(default, null): Bool;
     var keyType(default, null): DataType;
     var itemType(default, null): DataType;
-    var keyComparator(default, null): Comparator<Key>;
-    var itemComparator(default, null): Comparator<Item>;
-    var keyEquator(default, null): Equator<Key>;
-    var itemEquator(default, null): Equator<Item>;
+    var indexType(default, null): IndexType;
+    var options(default, null): IndexOptions<Key, Item>;
 
-/* === Methods === */
+    private function compareKeys(a:Key, b:Key):Int;
+    private function compareItems(a:Item, b:Item):Int;
+    private function getItemKey(item: Item):Key;
+    private function itemsEq(a:Item, b:Item):Bool;
 
-    function insertOne(doc: Item):Void;
-    function insertMany(docs: Array<Item>):Void;
-
-    function removeOne(doc: Item):Void;
-    function removeMany(docs: Array<Item>):Void;
-
+    function insertOne(item: Item):Void;
+    function removeOne(item: Item):Bool;
     function updateOne(oldDoc:Item, newDoc:Item):Void;
-    function updateMany(updates: Array<{oldDoc:Item, newDoc:Item}>):Void;
 
-    function revertUpdate(oldDoc:Item, newDoc:Item):Void;
-    function revertAllUpdates(updates: Array<{oldDoc:Item, newDoc:Item}>):Void;
+    function insertMany(items: Array<Item>):Void;
+    function removeMany(items: Array<Item>):Void;
+    function updateMany(updates: Array<{oldDoc:Item, newDoc:Item}>):Void;
 
     function getByKey(key: Key):Null<Array<Item>>;
     function getByKeys(keys: Array<Key>):Array<Item>;
-    function getBetweenBounds(?min:BoundingValue<Key>, ?max:BoundingValue<Key>):Array<Item>;
+    function getBetweenBounds(?min:KeyBoundary<Key>, ?max:KeyBoundary<Key>):Array<Item>;
     function getAll():Array<Item>;
 
-    function itrByKey(key: Key):Itr<Item>;
-    function itrByKeys(keys: Array<Key>):Itr<Item>;
-    function itrBetweenBounds(?min:BoundingValue<Key>, ?max:BoundingValue<Key>):Itr<Item>;
-    function itrAll():Itr<Item>;
-    function iterator():Itr<Item>;
-
+	function itrByKey(key:Key):Itr<Item>;
+	function itrByKeys(keys:Array<Key>):Itr<Item>;
+	function itrBetweenBounds(?min:KeyBoundary<Key>, ?max:KeyBoundary<Key>):Itr<Item>;
+	function itrAll():Itr<Item>;
+	function iterator():Iterator<Item>;
     function allKeys():Array<Key>;
     function keys():Iterator<Key>;
     function keyValueIterator():Iterator<IdxKvPair<Key, Item>>;
 
-    function getDocKey(doc: Item):Key;
-
-    function compareKeys(a:Key, b:Key):Int;
-    function compareItems(a:Item, b:Item):Int;
-
-    function keysEq(a:Key, b:Key):Bool;
-    function itemsEq(a:Item, b:Item):Bool;
+    function size():Int;
 }
 
 typedef IndexOptions<Key, Item> = {
-    fieldName: String,
-    ?fieldType: DataType,
+    ?type: IndexType,
+    ?fieldName: String,
+    ?getItemKey: Item -> Key,
+    ?keyType: DataType,
     ?keyComparator: Comparator<Key>,
     ?itemEquator: Equator<Item>,
-    ?getItemKey: Item -> Key,
+    ?keyOrdering: pm.Ord<Key>,
     ?unique: Bool,
     ?sparse: Bool
 };
@@ -73,4 +64,39 @@ class IdxKvPair<K, V> {
         key = k;
         value = v;
     }
+}
+
+class KeyBoundary<Key> {
+    public final key: Key;
+    public final inclusive: Bool;
+
+    public inline function new(key:Key, inclusive) {
+        this.key = key;
+        this.inclusive = inclusive;
+    }
+}
+
+class IndexRollback<Error> {
+	public inline function new(e, i) {
+		this.error = e;
+		this.failingIndex = i;
+	}
+
+	public var error:Error;
+	public var failingIndex:Int;
+}
+
+class IndexError<K, V> extends Error {
+    public var type(default, null): IndexErrorCode<K, V>;
+    public function new(?type:IndexErrorCode<K, V>, ?msg, ?name, ?pos) {
+        super(msg, name, pos);
+        this.type = nor(type, cast AssertionFailed);
+    }
+}
+
+enum IndexErrorCode<K, V> {
+	AssertionFailed;
+	UniqueConstraintViolated;
+	MissingProperty(name:String);
+	Custom(error: Error);
 }
